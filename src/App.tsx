9da9aabc68489deefad1ba8acb87c1f9d5062f3d
Luis <Luistejeda0011@gmail.com>
2023-03-21 13:42:33 -0300

    Setting project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+function App (): React.ReactElement<HTMLElement> {
+  return (
+    <div className='App h-screen flex flex-col items-center p-10'>
+      <p className='text-5xl font-black'>Sub List</p>
+    </div>
+  )
+}
+
+export default App
