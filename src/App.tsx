6e02ff60ceb8c2142690795e86cbe00fe559efba
Luis <Luistejeda0011@gmail.com>
2023-03-21 19:49:43 -0300

    Render Sub List and form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
-import './App.css'
+import useSubs from './hooks/useSubs'
+import List from './components/List'
+import Form from './components/Form'
 
 function App (): React.ReactElement<HTMLElement> {
+  const { subs, handleNewSub } = useSubs()
   return (
     <div className='App h-screen flex flex-col items-center p-10'>
       <p className='text-5xl font-black'>Sub List</p>
+      <div className='flex gap-5'>
+      <List subs={subs} />
+      <Form onNewSub={handleNewSub} />
+      </div>
     </div>
   )
 }
